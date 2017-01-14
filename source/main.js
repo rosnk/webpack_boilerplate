8cd6e92d80e2c4d296fb4d8291741fa675bba67b
rosnk <rosankarki@gmail.com>
2017-01-14 18:33:28 +0545

    Update main.js

--- source/main.js
+++ source/main.js
@@ -2,8 +2,11 @@
 require('./css/homepage.css');
 require('./css/shared.scss');
 
-var React = require('react');
-var ReactDOM = require('react-dom');
+//var React = require('react');
+//var ReactDOM = require('react-dom');
+
+import React from 'react';
+import ReactDOM from 'react-dom';
 
 var Header = require('./components/common/header');
 
